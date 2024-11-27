b394b2134c0db802fd3938d7f071e77ae0301cfa
bob <bob@>
2024-11-28 00:20:56 +0530

    Final code for password strength

--- index.js
+++ index.js
@@ -25,8 +25,45 @@ function checkStrength(password){
         strength: "",
         suggestions: [],
     }
-
-
+    var count = 0;
+    var passwordStrengthCount = 0;
+    if ((/^.{8,}$/).test(password)){
+        passwordStrengthCount ++;
+    } else {
+        passwordReview.suggestions[count] = "Password should contain minimum of 8 characters";
+        count ++;
+    }
+    if ((/(?=.*[A-Z])/).test(password)){
+        passwordStrengthCount ++;
+    } else {
+        passwordReview.suggestions[count] = "Password should contain atleast 1 capital letter";
+        count ++;
+    }
+    if ((/(?=.*[a-z])/).test(password)){
+        passwordStrengthCount ++;
+    } else {
+        passwordReview.suggestions[count] = "Password should contain atleast 1 small letter";
+        count ++;
+    }
+    if ((/.*\d/).test(password)){
+        passwordStrengthCount ++;
+    } else {
+        passwordReview.suggestions[count] = "Password should contain atleast 1 digit";
+    }
+    if ((/(?=.*[!@#$%^&*(){}\[\]<>,.?/`~\-+=_])/).test(password)){
+        passwordStrengthCount ++;
+    } else {
+        passwordReview.suggestions[count] = "Password should contain atleast 1 special character";
+        count ++;
+    }
+    if (passwordStrengthCount >= 0 && passwordStrengthCount <= 2){
+        passwordReview.strength = "Weak";
+    } else if (passwordStrengthCount > 2 && passwordStrengthCount <= 4){
+        passwordReview.strength = "Medium";
+    } else {
+        passwordReview.strength = "Strong";
+    }
+    return passwordReview;
 }
 
 module.exports = checkStrength;
--- password_strength.js
+++ password_strength.js
@@ -1,2 +1,17 @@
-var checkStrength = require('./index');
-console.log(checkStrength('123des#'));
+#!/usr/bin/env node
+
+// Import the password strength logic from index.js
+const checkStrength = require('./index');
+
+// Get the password from CLI arguments
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log("Usage: password-strength <password>");
+    process.exit(1);
+}
+
+const password = args[0];
+
+// Call the function and print the result
+const result = checkStrength(password);
+console.log(result);
