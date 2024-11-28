8c4ccf1d93ec70d3d4ee1d6efe1b96b23a4e9e98
bob <bob@>
2024-11-28 17:10:03 +0530

    Optimized the code and removed the comments

--- index.js
+++ index.js
@@ -1,64 +1,57 @@
 function checkStrength(password){
-    /*
-    var regularExpForProperPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-\_+={}[\]|<>.`~,?\/]).{8,}$/;
-    var regularExpForPasswordHavingLessThan8Chars = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-\_+={}[\]|<>.`~,?\/]).{0,7}$/;
-    var regularExpForPasswordNotHavingCapitalLetters = /^(?=.*[a-z])(?=.*[^A-Z\d!@#$%^&*()\-\+={}[\]|<>.`~,?\/]).{8,}$/;
-    var regularExpForPasswordNotHavingSmallLetters = /^(?=.*[^a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-\_+={}[\]|<>.`~,?\/]).{8,}$/;
-    var regularExpForPasswordNotHavingDigits = /^(?=.*[a-z])(?=.*[A-Z])(?=.*^\d)(?=.*[!@#$%^&*()\-\_+={}\[\]|<>.`~,?\/]).{8,}$/;
-    var regularExpForPasswordNotHavingSplCharacters = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^!@#$%\^&*()\-\_+={}\[\]|<>.`~,?\/]).{8,}$/;
-    if(regularExpForProperPassword.test(password)){ 
-        console.log("Strong password");
-    } else if(regularExpForPasswordHavingLessThan8Chars.test(password)){
-        console.log("Password should atleast be 8 characters long");
-    } else if(regularExpForPasswordNotHavingCapitalLetters.test(password)){
-        console.log("Password should contain atleast 1 capital letter");
-    } else if (regularExpForPasswordNotHavingSmallLetters.test(password)){
-        console.log("Password should have atleast 1 small letter");
-    } else if (regularExpForPasswordNotHavingDigits.test(password)){
-        console.log("Password should contain atleast 1 digit");
-    } else if (regularExpForPasswordNotHavingSplCharacters.test(password)){
-        console.log("Password should have atleast 1 special characters");
-    } else {
-        console.log("Weak password");
-    } */
+   
     var passwordReview = {
         strength: "",
         suggestions: [],
     }
     var count = 0;
+    var totalIfCases = 0;
     var passwordStrengthCount = 0;
     if ((/^.{8,}$/).test(password)){
         passwordStrengthCount ++;
+        totalIfCases ++;
     } else {
         passwordReview.suggestions[count] = "Password should contain minimum of 8 characters";
         count ++;
+        totalIfCases ++;
     }
     if ((/(?=.*[A-Z])/).test(password)){
         passwordStrengthCount ++;
+        totalIfCases ++;
     } else {
         passwordReview.suggestions[count] = "Password should contain atleast 1 capital letter";
         count ++;
+        totalIfCases ++;
     }
-    if ((/(?=.*[a-z])/).test(password)){
+    //if ((/(?=.*[a-z])/).test(password)){
+    if ((/[a-z]/).test(password)){
         passwordStrengthCount ++;
+        totalIfCases ++;
     } else {
         passwordReview.suggestions[count] = "Password should contain atleast 1 small letter";
         count ++;
+        totalIfCases ++;
     }
     if ((/.*\d/).test(password)){
         passwordStrengthCount ++;
+        totalIfCases ++;
     } else {
         passwordReview.suggestions[count] = "Password should contain atleast 1 digit";
+        totalIfCases ++;
     }
-    if ((/(?=.*[!@#$%^&*(){}\[\]<>,.?/`~\-+=_])/).test(password)){
+    //if ((/(?=.*[!@#$%^&*(){}\[\]<>,.?/`~\-+=_])/).test(password)){
+    if ((/(?= .*\W)/).test(password)){
         passwordStrengthCount ++;
+        totalIfCases ++;
     } else {
         passwordReview.suggestions[count] = "Password should contain atleast 1 special character";
         count ++;
+        totalIfCases ++;
     }
-    if (passwordStrengthCount >= 0 && passwordStrengthCount <= 2){
+    console.log(totalIfCases);
+    if (passwordStrengthCount >= 0 && passwordStrengthCount <= Math.floor(totalIfCases/2)){
         passwordReview.strength = "Weak";
-    } else if (passwordStrengthCount > 2 && passwordStrengthCount <= 4){
+    } else if (passwordStrengthCount > Math.floor(totalIfCases/2) && passwordStrengthCount < totalIfCases ){
         passwordReview.strength = "Medium";
     } else {
         passwordReview.strength = "Strong";
