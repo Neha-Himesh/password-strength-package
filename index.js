c542cfb3719298104c5250761dc1f17dd5503714
bob <bob@>
2024-12-10 11:44:27 +0530

    Modified the code by adding password_suggestions.js for suggestions

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
+var passwordSuggestionsUsingLengthsVariables = require('./password_suggestions');
+
 function checkStrength(password, language, caseSensitive, minLength, maxLength){
-   //https://neha-himesh.github.io/password-strength-package/
+   
     var passwordReview = {
+        booleanValue: "",
         strength: "",
         suggestions: [],
     }
     var totalIfCases = 0;
     var passwordStrengthCount = 0;
-    const regex = new RegExp(`^.{${minLength},${maxLength}}$`);
+    // regular expression to check minimum and maximum characters length
+    const regex = new RegExp(`^.{${minLength},${maxLength}}$`, 'u');
+    // regular expression to check existence of space/puntuation/special character/emoji
     const regexSplCharacters = /[\s\p{P}\p{S}\p{So}]/gu;
+
+    const passwordSuggestions = passwordSuggestionsUsingLengthsVariables(minLength, maxLength);
+    //Checking if length has minimum and maximum characters length based on user's choice
     if ((regex).test(password)){
         passwordStrengthCount ++;
         totalIfCases ++;
     } else {
-        passwordReview.suggestions.push(`Password should contain minimum of ${minLength} characters and maximum of ${maxLength} characters`);
+        passwordReview.suggestions.push(passwordSuggestions.lengthCheck);
         totalIfCases ++;
     }
     if (caseSensitive === true){
-        // Matches uppercase letters
+        //Checking if there is atleast 1 captial letter
         if ((/\p{Lu}/u).test(password)){
             passwordStrengthCount ++;
             totalIfCases ++;
         } else {
-            passwordReview.suggestions.push("Password should contain atleast 1 capital letter");
+            passwordReview.suggestions.push(passwordSuggestions.capitalLetterCheck);
             totalIfCases ++;
         }
-        // Matches lowercase letters
+
+        //Checking if there is atleast 1 small letter
         if ((/\p{Ll}/u).test(password)){
             passwordStrengthCount ++;
             totalIfCases ++;
         } else {
-            passwordReview.suggestions.push("Password should contain atleast 1 small letter");
+            passwordReview.suggestions.push(passwordSuggestions.smallLetterCheck);
             totalIfCases ++;
         }
     } else {
@@ -37,33 +46,37 @@ function checkStrength(password, language, caseSensitive, minLength, maxLength){
             passwordStrengthCount ++;
             totalIfCases ++;
         } else {
-            passwordReview.suggestions.push("Password should contain atleast 1 letter");
+            passwordReview.suggestions.push(passwordSuggestions.letterCheck);
             totalIfCases ++;
         }               
-    }
-    
+    }    
+
+    //Checking if there is atleast 1 digit
     if ((/\p{N}/u).test(password)){
         passwordStrengthCount ++;
         totalIfCases ++;
     } else {
-        passwordReview.suggestions.push("Password should contain atleast 1 digit");
+        passwordReview.suggestions.push(passwordSuggestions.digitCheck);
         totalIfCases ++;
     }
-    // Matches spaces, punctuation, symbols, or emojis
+    
+    //Checking if there is atleast 1 special character
     if (regexSplCharacters.test(password)){
         passwordStrengthCount ++;
         totalIfCases ++;
     } else {
-        passwordReview.suggestions.push("Password should contain atleast 1 special character, space or an emoji");
+        passwordReview.suggestions.push(passwordSuggestions.specialCharacterCheck);
         totalIfCases ++;
     }
-    
     if (passwordStrengthCount >= 0 && passwordStrengthCount <= Math.floor(totalIfCases/2)){
-        passwordReview.strength = "Weak";
+        passwordReview.strength = 0;
+        passwordReview.booleanValue = false;
     } else if (passwordStrengthCount > Math.floor(totalIfCases/2) && passwordStrengthCount < totalIfCases ){
-        passwordReview.strength = "Medium";
+        passwordReview.strength = 1;
+        passwordReview.booleanValue = false;
     } else {
-        passwordReview.strength = "Strong";
+        passwordReview.strength = 2;
+        passwordReview.booleanValue = true;
     }
     return passwordReview;
 }
---
+++ password_suggestions.js
@@ -0,0 +1,10 @@
+passwordSuggestions = (minLength, maxLength) => ({
+    lengthCheck: `Password should contain minimum of ${minLength}, and maximum of ${maxLength} characters`,
+    capitalLetterCheck: "Password should contain atleast 1 capital letter",
+    smallLetterCheck: "Password should contain atleast 1 small letter",
+    digitCheck: "Password should contain atleast 1 digit",
+    specialCharacterCheck: "Password should contain atleast 1 special character",
+    letterCheck: "Password should contain atleast 1 letter",
+});
+
+module.exports = passwordSuggestions;
