c8fa81949e1627f290ac5e49571e1ecb358aa551
Neha-Himesh <neha.himesh@spike.sh>
2024-12-23 14:14:00 +0530

    Modified suggestions for spl characters,space and emoji check

--- password_suggestions.js
+++ password_suggestions.js
@@ -3,7 +3,7 @@ passwordSuggestions = (minLength, maxLength) => ({
     capitalLetterCheck: "Password should contain atleast 1 capital letter",
     smallLetterCheck: "Password should contain atleast 1 small letter",
     digitCheck: "Password should contain atleast 1 digit",
-    specialCharacterCheck: "Password should contain atleast 1 special character",
+    specialCharacterCheck: "Password should contain atleast 1 special character, space or an emoji",
     letterCheck: "Password should contain atleast 1 letter",
 });
 
