80175bbf132f3c8c6788f590b20f43be498c0ae9
Neha-Himesh <neha.himesh@spike.sh>
2025-01-24 21:06:33 +0530

    Merge conflict before the ...d1 commit

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,28 @@ const path = require('path');
 
 const app = express();
 
-// Set up handlebars as the templating engine
-// app.engine('hbs', expressHbs.engine({
-//     extname: '.hbs',
-//     layoutsDir: path.join(__dirname, '../views/layouts'), // Path to layouts directory
-//     defaultLayout: 'main', // Specifies 'main.hbs' as the default layout
-// }));
-// app.set('view engine', 'hbs');
-// app.set('views', path.join(__dirname, '../views')); // Ensure your index.hbs is inside a 'views' directory
+//Set up handlebars as the templating engine
+app.engine('hbs', expressHbs.engine({
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, '../views/layouts'), // Path to layouts directory
+    defaultLayout: 'main', // Specifies 'main.hbs' as the default layout
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views')); // Ensure your index.hbs is inside a 'views' directory
 
 // Middleware to serve static files (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../dist'))); // For precompiled files
+// app.use(express.static(path.join(__dirname, '../dist'))); // For precompiled files
 
 // Route to serve the main page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/main.html')); // Adjust filename as needed
+    res.render('main_page'); // Adjust filename as needed
 });
 
-// Catch-all route to serve the index.html for unmatched routes (if needed)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/main.html"));
-});
+// // Catch-all route to serve the index.html for unmatched routes (if needed)
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist/main.html"));
+// });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
