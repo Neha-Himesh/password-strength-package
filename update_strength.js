21e96bd507aa300332096b36e62f8c2b53a4a6d2
Neha-Himesh <neha.himesh@spike.sh>
2025-01-28 00:07:43 +0530

    Added another slide of carousel and added update_strength.js file

---
+++ update_strength.js
@@ -0,0 +1,21 @@
+const checkStrength = require("./index");
+
+function updateStrength() {
+    console.log("Password check");
+    event.preventDefault(); // Prevent form from reloading the page
+    var password = document.getElementById("password").value;
+    var languageCaseSensitivity = isCaseSensitive(passwordLanguage); //document.getElementById("case-sensitive-input").value === "true";
+    var strengthInfo = checkStrength(password, languageCaseSensitivity, 8, 15);
+    var strengthText = `Strength: ${strengthInfo.strength}`;
+
+    if (strengthInfo.suggestions.length > 0) {
+        strengthText += `<br><strong>Suggestions:</strong><ul>`;
+        strengthInfo.suggestions.forEach(suggestion => {
+            strengthText += `<li>${suggestion}</li>`;
+        });
+        strengthText += `</ul>`;
+    }
+
+    document.getElementById("strength").innerHTML = strengthText;
+}
+module.exports = updateStrength;
