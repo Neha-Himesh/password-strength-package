53330abac2acbbaa1f2f5f8050b291c7f7e246ed
Neha-Himesh <neha.himesh@spike.sh>
2025-01-28 13:32:35 +0530

    Added all js code to one file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import checkStrength from './check_strength.js';
+// import checkStrength from './check_strength.js';
 
 function updateStrength() {
     console.log("Password check");
@@ -19,4 +19,138 @@ function updateStrength() {
     document.getElementById("strength").innerHTML = strengthText;
 }
 
-export default updateStrength;
+function checkStrength(password, caseSensitive, minLength, maxLength){
+   
+    var passwordReview = {
+        booleanValue: "",
+        strength: "",
+        suggestions: [],
+    }
+    var totalIfCases = 0;
+    var passwordStrengthCount = 0;
+    // regular expression to check minimum and maximum characters length
+    const regex = new RegExp(`^.{${minLength},${maxLength}}$`, 'u');
+    // regular expression to check existence of space/puntuation/special character/emoji
+    const regexSplCharacters = /[\s\p{P}\p{S}\p{So}]/gu;
+
+    const passwordSuggestions = passwordSuggestionsUsingLengthsVariables(minLength, maxLength);
+    //Checking if length has minimum and maximum characters length based on user's choice
+    if ((regex).test(password)){
+        passwordStrengthCount ++;
+        totalIfCases ++;
+    } else {
+        passwordReview.suggestions.push(passwordSuggestions.lengthCheck);
+        totalIfCases ++;
+    }
+    if (caseSensitive === true){
+        //Checking if there is atleast 1 captial letter
+        if ((/\p{Lu}/u).test(password)){
+            passwordStrengthCount ++;
+            totalIfCases ++;
+        } else {
+            passwordReview.suggestions.push(passwordSuggestions.capitalLetterCheck);
+            totalIfCases ++;
+        }
+
+        //Checking if there is atleast 1 small letter
+        if ((/\p{Ll}/u).test(password)){
+            passwordStrengthCount ++;
+            totalIfCases ++;
+        } else {
+            passwordReview.suggestions.push(passwordSuggestions.smallLetterCheck);
+            totalIfCases ++;
+        }
+    } else {
+        if ((/\p{L}/u).test(password)){
+            passwordStrengthCount ++;
+            totalIfCases ++;
+        } else {
+            passwordReview.suggestions.push(passwordSuggestions.letterCheck);
+            totalIfCases ++;
+        }               
+    }    
+
+    //Checking if there is atleast 1 digit
+    if ((/\p{N}/u).test(password)){
+        passwordStrengthCount ++;
+        totalIfCases ++;
+    } else {
+        passwordReview.suggestions.push(passwordSuggestions.digitCheck);
+        totalIfCases ++;
+    }
+    
+    //Checking if there is atleast 1 special character
+    if (regexSplCharacters.test(password)){
+        passwordStrengthCount ++;
+        totalIfCases ++;
+    } else {
+        passwordReview.suggestions.push(passwordSuggestions.specialCharacterCheck);
+        totalIfCases ++;
+    }
+    if (passwordStrengthCount >= 0 && passwordStrengthCount <= Math.floor(totalIfCases/2)){
+        passwordReview.strength = 0;
+        passwordReview.booleanValue = false;
+    } else if (passwordStrengthCount > Math.floor(totalIfCases/2) && passwordStrengthCount < totalIfCases ){
+        passwordReview.strength = 1;
+        passwordReview.booleanValue = false;
+    } else {
+        passwordReview.strength = 2;
+        passwordReview.booleanValue = true;
+    }
+    return passwordReview;
+}
+
+passwordSuggestions = (minLength, maxLength) => ({
+    lengthCheck: `Password should contain minimum of ${minLength}, and maximum of ${maxLength} characters`,
+    capitalLetterCheck: "Password should contain atleast 1 capital letter",
+    smallLetterCheck: "Password should contain atleast 1 small letter",
+    digitCheck: "Password should contain atleast 1 digit",
+    specialCharacterCheck: "Password should contain atleast 1 special character, space or an emoji",
+    letterCheck: "Password should contain atleast 1 letter",
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetching main sub-menu of the dropdown
+    const tableOfContentsMainSection = document.getElementById('about');
+    // Fetching all the elements which are submenus of the product dropdown
+    if(tableOfContentsMainSection.classList.contains('d-none')){
+        tableOfContentsMainSection.classList.remove('d-none');
+    }
+    const tableOfContentsDocumentationSections = document.querySelectorAll('.table-of-contents-home-page-documentation[data-target]');
+    // Fetching main menu of the dropdown
+    const tableOfContentsDocumentationList = document.getElementById('table-of-contents-documentation-list');
+ 
+    // Assigning previous element which was supposed to be displayed in the sub-menu as main sub-menu
+    let elementHavingPreviousTargetId = tableOfContentsMainSection;
+
+    // On mouse enter event, fetching the target data of the sub-menu element and finding the element with the matching target ID
+    tableOfContentsDocumentationSections.forEach(option => {
+        option.addEventListener('click', function () {
+            const targetIdOfSubSection = this.dataset.target;
+            const elementWithRequiredTargetId = document.getElementById(targetIdOfSubSection);
+            // Adding 'd-none' class to the previous target element and removing 'd-none' from the required target ID (sub-menu)
+            if (elementHavingPreviousTargetId) {
+                elementHavingPreviousTargetId.classList.add('d-none');
+            }
+            // Assigning the required target ID to the previous target ID to save the data for the next hover
+            if (elementWithRequiredTargetId) {
+                elementWithRequiredTargetId.classList.remove('d-none');
+                elementHavingPreviousTargetId = elementWithRequiredTargetId;
+            }
+        });
+    });
+
+
+
+//     // When mouse leaves the main menu, remove the 'd-none' class from the main sub-menu
+//     tableOfContentsDocumentationList.addEventListener('', function () {
+//         tableOfContentsMainSection.classList.remove('d-none');
+//         // Adding 'd-none' class to the previous target ID, if it's not the main sub-menu
+//         if (elementHavingPreviousTargetId !== tableOfContentsMainSection) {
+//             elementHavingPreviousTargetId.classList.add('d-none');
+//             elementHavingPreviousTargetId = tableOfContentsMainSection;
+//         }
+//     });
+});
+
+// export default updateStrength;
